7e93e851fa9fed3481fa6635026d364085579e34
Mutasim <mutasim.business2020@gmail.com>
2025-08-03 18:43:07 +0300

    feat: configer the auth service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,25 @@
 import express from 'express';
+import cors from 'cors';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const app = express();
+app.use(
+    cors({
+        origin: ['http://localhost:3000'],
+        allowedHeaders: ['Authorization', 'Content-Type'],
+        credentials: true
+    })
+)
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API'});
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
-});
+const server = app.listen(port, ()=>{
+    console.log(`Auth service is running at http://localhost:${port}/api`);
+})
+ 
+server.on('error', (err)=>{
+    console.log(`Auth Server Error: ${err}`);
+})
