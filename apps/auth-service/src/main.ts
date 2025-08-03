815ba33803a95bf17006c2d47b53140f170d664e
Mutasim <mutasim.business2020@gmail.com>
2025-08-03 20:35:32 +0300

    feat: add error middleware handler to auth service

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { errorMiddleware } from '../../../packages/error-handler/error-middleware';
+import cookieParser from 'cookie-parser';
 
 const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
@@ -11,6 +13,10 @@ app.use(
         credentials: true
     })
 )
+
+app.use(cookieParser())
+app.use(express.json())
+app.use(errorMiddleware)
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API'});
