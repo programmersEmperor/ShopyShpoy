3311fd939547bbbe61dd0f19a17c1f282b1c6a3a
Mutasim <mutasim.business2020@gmail.com>
2025-08-03 18:42:24 +0300

    feat: configer the api-gateway Service

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -4,18 +4,61 @@
  */
 
 import express from 'express';
-import * as path from 'path';
+import cors from 'cors';
+import morgan from 'morgan';
+import proxy from 'express-http-proxy';
+import rateLimit, { ipKeyGenerator } from 'express-rate-limit';
+// import swaggerUi from 'swagger-ui-express';
+// import axios from 'axios';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+// CORS CONFIG
+app.use(
+  cors({
+    origin: ['http://localhost:3000'], 
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true,
+  })
+)
 
-app.get('/api', (req, res) => {
+// LOGS CONFIG
+app.use(morgan("dev"))
+
+// MAX UPLOAD SIZE CONFIG
+app.use(express.json({limit: "100mb"}))
+app.use(express.urlencoded({limit: "100mb", extended: true}))
+
+// COOKIE CONFIG
+app.use(cookieParser())
+
+app.set("trust proxy", 1)
+
+const limiter = rateLimit({
+  // 15 minutes * 60 seconds * 1000 milliseconds
+  windowMs: 15 * 60 * 1000,
+  limit: (req: any) => (req.user ? 1000 : 100),
+  message: { 
+    error: 'too many requests, please try again later!'
+  },
+  standardHeaders: true,
+  legacyHeaders: true,
+
+  keyGenerator: (req: any) => ipKeyGenerator(req.ip),
+})
+
+app.use(limiter)
+
+
+app.get('/gateway-health', (req, res) => {
   res.send({ message: 'Welcome to api-gateway!' });
 });
 
-const port = process.env.PORT || 3333;
+app.use('/', proxy('http://localhost:6001'))
+
+const port = process.env.PORT || 8080;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}/gateway-health`);
 });
 server.on('error', console.error);
