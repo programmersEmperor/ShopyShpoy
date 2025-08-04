6c83e64c6bc168ba49f5f15dae384f9b5fd07246
Mutasim <mutasim.business2020@gmail.com>
2025-08-04 20:59:10 +0300

    feat: declare the auth controller

---
+++ apps/auth-service/src/controller/auth.controller.ts
@@ -0,0 +1,6 @@
+import { NextFunction, Request, Response } from "express";
+
+// REGISTER A NEW USER
+export const userRegistration = async (request: Request, response: Response, next: NextFunction)=>{
+
+}
